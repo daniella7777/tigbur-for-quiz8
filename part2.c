6e45b1fbae17f9d26090c2c51c7ec0d4d9d0bf1f
daniella7777 <126858452+daniella7777@users.noreply.github.com>
2025-01-20 13:54:31 +0000

    correct part2

---
+++ part2.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main() 
+{ 
+    char str[50]; 
+   
+    //open file example2.txt in write mode with fopen
+   
+   // get a name from the user using only fprintf and fscanf and write it to the file
+    
+    //close the file
+   
+    return 0; 
+}
